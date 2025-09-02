795233ed9742f67bba62fe653e1cbabbdcef4328
Abu Turab <turababu989@gmail.com>
2025-09-02 23:36:40 +0530

    Added Password Encryption

--- services/auth.services.controller.js
+++ services/auth.services.controller.js
@@ -2,6 +2,7 @@ import { db } from "../config/db-client.js";
 import { eq , and } from "drizzle-orm";
 import { users } from "../drizzle/schema.js";
 import bcrypt from "bcrypt";
+import argon2 from "argon2";
 
 export const getUserByEmail = async (email) => {
     return await db.select().from(users).where(eq(users.email , email));
@@ -14,9 +15,11 @@ export const createUser = async ({ name , email , password}) => {
 }
 
 export const getHashPassword = async (password) => {
-    return await bcrypt.hash(password , 10 );
+    // return await bcrypt.hash(password , 10 );
+    return await argon2.hash(password);
 }
 
 export const comparePassword = async (password , hashPassword) => {
-    return await bcrypt.compare(password , hashPassword);
+    // return await bcrypt.compare(password , hashPassword);
+    return await argon2.verify( hashPassword , password );
 }
