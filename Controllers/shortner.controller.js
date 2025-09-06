a5e1a089643dea759f7856f801818c5e28b09e56
Abu Turab <turababu989@gmail.com>
2025-09-06 15:47:20 +0530

    Added Flash message on home page

--- Controllers/shortner.controller.js
+++ Controllers/shortner.controller.js
@@ -1,34 +1,36 @@
+
 import { saveToFile , getLinks , getShortLinks} from "../services/services.controller.js";
+import { shortnerValidation } from "../validation/shortner-validation.js";
 
 
 export const getShortenURL = async (req, res) => {
   if(!req.user) return res.redirect("/register");
   const Links = await getLinks(req.user.id);
   
-  res.render("index", { Links, host: req.host });
+  res.render("index", { Links, host: req.host , errors: req.flash("errors") , success : req.flash("success")});
 };
 
 export const shortener = async (req, res) => {
-  const { url , shortCode } = req.body;
+  const { data , error } = shortnerValidation.safeParse(req.body);
+  if(error){
+    const errors = error.issues[0].message;
+    req.flash("errors" , errors);
+   return res.redirect("/");
+  }
+
+  const { url , shortCode } = data;
  const Links = await getShortLinks(shortCode);
 
  
   if (Links.length > 0) {
-    return res.send(`
-    <script>
-      alert("Short code already exists. Try another one!");
-      window.location.href = "/";
-    </script>
-  `);
+    req.flash("errors" , "Short code already exists. Try another one!");
+    return res.redirect("/");
   }
 
   await saveToFile({ url, shortCode , userId: req.user.id});
-  res.send(`
-    <script>
-      alert("URL shorten successfully!");
-      window.location.href = "/";
-    </script>
-  `);
+  req.flash("success" , "URL shorten successfully!");
+  return res.redirect("/");
+  
 };
 
 
